14f436c3116227451a89cbc6b84e12ba0294bd5a
Je Hon Tan <jehontan@gmail.com>
2025-10-20 17:56:57 -0500

    add convenience CMake functions to add plugins

---
+++ behaviortree_ros2/cmake/plugins.cmake
@@ -0,0 +1,73 @@
+function(add_behavior_plugin)
+    set(options "")
+    set(oneValueArgs NAME)
+    set(multiValueArgs SOURCES DEPENDENCIES)
+
+    cmake_parse_arguments("ARG" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    add_library(${ARG_NAME} SHARED
+        ${ARG_SOURCES}
+    )
+
+    target_compile_definitions(${ARG_NAME} PRIVATE BT_PLUGIN_EXPORT)
+
+    target_include_directories(${ARG_NAME}
+    PUBLIC
+        "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>"
+        "$<INSTALL_INTERFACE:include/${PROJECT_NAME}>"
+    )
+
+    ament_target_dependencies(${ARG_NAME}
+        rclcpp
+        behaviortree_cpp
+        behaviortree_ros2
+        ${ARG_DEPENDENCIES}
+    )
+
+    install(
+        TARGETS ${ARG_NAME}
+        ARCHIVE DESTINATION share/${PROJECT_NAME}/behaviors
+        LIBRARY DESTINATION share/${PROJECT_NAME}/behaviors
+        RUNTIME DESTINATION share/${PROJECT_NAME}/behaviors
+    )
+
+    ament_export_libraries(${ARG_NAME})
+
+endfunction()
+
+function(add_condition_plugin)
+    set(options "")
+    set(oneValueArgs NAME)
+    set(multiValueArgs SOURCES DEPENDENCIES)
+
+    cmake_parse_arguments("ARG" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    add_library(${ARG_NAME} SHARED
+        ${ARG_SOURCES}
+    )
+
+    target_compile_definitions(${ARG_NAME} PRIVATE BT_PLUGIN_EXPORT)
+
+    target_include_directories(${ARG_NAME}
+    PUBLIC
+        "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>"
+        "$<INSTALL_INTERFACE:include/${PROJECT_NAME}>"
+    )
+
+    ament_target_dependencies(${ARG_NAME}
+        rclcpp
+        behaviortree_cpp
+        behaviortree_ros2
+        ${ARG_DEPENDENCIES}
+    )
+
+    install(
+        TARGETS ${ARG_NAME}
+        ARCHIVE DESTINATION share/${PROJECT_NAME}/conditions
+        LIBRARY DESTINATION share/${PROJECT_NAME}/conditions
+        RUNTIME DESTINATION share/${PROJECT_NAME}/conditions
+    )
+
+    ament_export_libraries(${ARG_NAME})
+
+endfunction()
